a85bfa03a53b9b0d65f6167c7575feea33b6b391
jorgemonraz <jmonraz@student.fullsail.edu>
2023-07-07 15:25:45 -0700

    added validation to diabetes prediction model view to avoid creating multiple diabetes analysis for same patient

--- prediction_model/views.py
+++ prediction_model/views.py
@@ -4,30 +4,35 @@ from app.models import Patient, DiabetesAnalysis
 import numpy as np
 import joblib
 
+
 class ModelDiabetesPrediction(APIView):
 
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-    
+
     def post(self, request):
         # extract and preprocess input data
         patient_data = request.data.get('patient')
         diabetes_data = request.data.get('data')
 
-        preprocessed_data = preprocess_data(diabetes_data, patient_data.get('id'))
+        preprocessed_data = preprocess_data(
+            diabetes_data, patient_data.get('id'))
         # load the trained model
         model = joblib.load('./ml_model/diabetes_model.pk1')
 
         try:
+            # check if diabetes analysis already exists for the patient ID
+            if DiabetesAnalysis.objects.filter(patient_id=patient_data.get('id')).exists():
+                return Response({"message": "Diabetes analysis already exists for this patient"})
+
             # make predictions
             prediction = model.predict(preprocessed_data)
             outcome = int(prediction[0])
 
-            #prepare data
+            # prepare data
 
             analysis_data = {
                 'glucose': diabetes_data.get('glucose'),
-                'blood_pressure': diabetes_data.get('blood_pressure'),
                 'insulin': diabetes_data.get('insulin'),
                 'bmi': diabetes_data.get('bmi'),
                 'patient_id': patient_data.get('id'),
@@ -36,10 +41,11 @@ class ModelDiabetesPrediction(APIView):
             # create diabetesanalysis object with prediction
             analysis = DiabetesAnalysis.objects.create(**analysis_data)
             analysis.save()
-            
+
             return Response({"message": "Analysis successful"})
         except Exception as e:
             return Response({'error': str(e)}, status=500)
+
 
 def preprocess_data(data, patient_id):
     patient = Patient.objects.get(id=patient_id)
@@ -51,7 +57,6 @@ def preprocess_data(data, patient_id):
 
     array_2d = np.array([[glucose, blood_pressure, insulin, bmi, age]])
 
-    array_2d = array_2d.reshape((1,-1))
+    array_2d = array_2d.reshape((1, -1))
 
     return array_2d
-
